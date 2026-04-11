86edb25b08360c038be4adca4d7094a33129d66d
Juan Sueldo <juan.sueldo1989@gmail.com>
2026-04-11 08:40:26 -0300

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors({
+  origin: [
+    'http://localhost:4321', // Astro en desarrollo
+    'https://juansueldo.dev' // tu web en producción
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json());
 
